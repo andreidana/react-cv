ed480db94f1b67b0bd3c5fa006e0417ad3b30cfd
Andrei Dana <andrei.dana87@gmail.com>
2019-08-05 22:32:41 +0300

    fix linter issues

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+import ResumeContainer from './ResumeContainer/ResumeContainer';
+
+function App() {
+  return (
+    <ResumeContainer />
+  );
+}
+
+export default App;
---
+++ src/ResumeContainer/ResumeContainer.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ResumeComponent extends Component {
+
+  render() {
+    return (
+      <div>Hello React Component</div>
+    );
+  }
+}
+
+export default ResumeComponent;
