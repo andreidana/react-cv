81204b1ba28d6c3b73cab617fbb621a003c277d7
Andrei Dana <andrei.dana87@gmail.com>
2019-08-14 23:14:27 +0300

    start work on attachment components

--- src/Components/Attachment/Attachment.jsx
+++ src/Components/Attachment/Attachment.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faCloudDownloadAlt } from '@fortawesome/free-solid-svg-icons';
+import './Attachment.module.scss';
 
-const Attachment = () => {
-  return (
-    <>
-      
-    </>
-  );
-};
+const Attachment = (props) => (
+  <>
+    <li className = 'li-style'>  
+      <p>
+        <img src = '{props.image}' alt = ''/>
+        {props.title}
+        <a href='{props.downloadPdf}'><FontAwesomeIcon icon={faEye} /></a>
+        <a href='{props.downloadWord}'><FontAwesomeIcon icon={faCloudDownloadAlt} /></a>
+      </p>
+    </li>
+  </>
+);
 
 export default Attachment;
--- src/Containers/AttachmentsContainer/AttachmentsContainer.jsx
+++ src/Containers/AttachmentsContainer/AttachmentsContainer.jsx
@@ -1,14 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
+import pdf from '../../Assets/pdf-icon.jpg';
+import word from '../../Assets/word-icon.jpg';
+import Attachment from '../../Components/Attachment/Attachment';
+import './AttachmentsContainer.module.scss';
 
-export class AttachmentsContainer extends Component {
-  render() {
-    return (
-      <>
-        <div>Attachment 1</div>
-        <div>Attachment 2</div>
-      </>
-    );
-  }
-}
+const AttachmentsContainer = () => {
+  const pdfTitle = 'Curriculum Vitae.pdf';
+  const wordTitle = 'Curriculum Vitae.docx';
+
+  return (
+    <>
+      <ul className = 'list-style-none'>  
+        <Attachment image = {pdf} title = {pdfTitle}/>
+        <Attachment image = {word} title = {wordTitle}/>
+      </ul>
+    </>
+  );
+};
 
 export default AttachmentsContainer;
