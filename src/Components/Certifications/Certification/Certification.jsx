e656b854bc438340ef8bd4b67ee34764f97cb4cf
Andrei Dana <andrei.dana87@gmail.com>
2019-09-23 22:20:22 +0300

    arrange certifications screen

--- src/Components/Certifications/Certification/Certification.jsx
+++ src/Components/Certifications/Certification/Certification.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import pdf from '../../../Assets/pdf-icon.jpg';
 import Attachment from '../../Attachment/Attachment';
 import Image from '../../Image/Image';
+import { ListStyle } from './Certification.module.scss';
 
 const Certification = props => {
   return (
-    <div>
+    <div className='mt-2'>
       <div className='row'>
         <div className='col-md-2'>
           <Image path = {props.logo} height = {50}></Image>
@@ -14,11 +15,11 @@ const Certification = props => {
           <div className="row">{props.name}</div>
           <div className="row">{props.emitent}</div>
         </div>
-        <div className='col-md-2'>{props.dateAchieved}</div>
+        <div className='col-md-2 mt-3'>{props.dateAchieved}</div>
       </div>
       <br/>
-      <div className='row'><p>{props.description}</p></div>
-      <div className='row'><Attachment image = {pdf} title = {props.name}/></div>
+      <div className='container'><p>{props.description}</p></div>
+      <div className='container mb-2'><ul className={ ListStyle }><Attachment image = {pdf} title = {props.name}/></ul></div>
     </div>
   );
 };
--- src/Containers/AttachmentsContainer/AttachmentsContainer.jsx
+++ src/Containers/AttachmentsContainer/AttachmentsContainer.jsx
@@ -4,13 +4,13 @@ import word from '../../Assets/word-icon.jpg';
 import Attachment from '../../Components/Attachment/Attachment';
 import { AttachmentsContainerStyle } from './AttachmentsContainer.module.scss';
 
-const AttachmentsContainer = () => {
+const AttachmentsContainer = props => {
   const pdfTitle = 'Curriculum Vitae.pdf';
   const wordTitle = 'Curriculum Vitae.docx';
 
   return (
     <div className = { AttachmentsContainerStyle }>
-      <ul>  
+      <ul>
         <Attachment image = {pdf} title = {pdfTitle}/>
         <Attachment image = {word} title = {wordTitle}/>
       </ul>
